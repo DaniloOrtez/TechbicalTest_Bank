974a2b975718095e549867e440b0b1315ccb7433
Danilo Morales <danmar.ortez@gmail.com>
2022-01-07 16:07:05 -0600

    Adding methods in Hooks, WebDriverFactory and CreateDriver

--- src/test/java/Functions/WebDriverFactory.java
+++ src/test/java/Functions/WebDriverFactory.java
@@ -1,4 +1,74 @@
 package Functions;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class WebDriverFactory {
+
+    private static Properties prop = new Properties();
+    private static InputStream in = CreateDriver.class.getResourceAsStream( "../test.properties");
+    private static String resourceFolder;
+
+    private static WebDriverFactory instance = null;
+
+    public static WebDriver CreateNewWebDriver(String browser, String os) throws IOException {
+        WebDriver driver;
+        prop.load(in);
+        resourceFolder = prop.getProperty("resourceFolder");
+
+        //******** If the driver selected is Firefox  ********//
+        if ("FIREFOX".equalsIgnoreCase(browser)) {
+            if("WINDOWS".equalsIgnoreCase(os)){
+                WebDriverManager.firefoxdriver().setup();
+            }
+            else{
+                System.out.println("The Driver is not selected properly, invalid name: " + browser + ", " + os);
+                return null;
+            }
+            driver = new FirefoxDriver();
+        }
+
+        //******** If the driver selected is Chrome  ********//
+        else if ("CHROME".equalsIgnoreCase(browser)) {
+            if("WINDOWS".equalsIgnoreCase(os)){
+                WebDriverManager.chromedriver().setup();
+            }
+            else{
+                System.out.println("The Driver is not selected properly, invalid name: " + browser + ", " + os);
+                return null;
+            }
+            driver = new ChromeDriver();
+
+        }
+
+        //******** if the driver selected is Edge ********//
+        else if ("Edge".equalsIgnoreCase(browser)) {
+            if ("WINDOWS".equalsIgnoreCase(os)) {
+                WebDriverManager.edgedriver().setup();
+            }
+            else {
+                System.out.println("The Driver is not selected properly, invalid name: " + browser + ", " + os);
+                return null;
+            }
+            driver = new EdgeDriver();
+        }
+
+        //******** If the driver is not selected  ********//
+        else {
+            System.out.println("The Driver is not selected properly, invalid name: " + browser + ", " + os);
+            return null;
+        }
+
+        driver.manage().window().maximize();
+        return driver;
+
+    }
+
 }
